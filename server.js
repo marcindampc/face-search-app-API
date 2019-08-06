aac36e17cf374507c9a7cf55fa3449b501a7517b
Marcin Dampc <marcindampc@gmail.com>
2019-08-06 03:57:53 +0100

    create new user by POST register route

--- server.js
+++ server.js
@@ -44,7 +44,19 @@ app.post('/signin', (req, res) => {
   } else {
     res.status(400).json('error logged in');
   }
+})
 
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json('new user added')
 })
 
 
