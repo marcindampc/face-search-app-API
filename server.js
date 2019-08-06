61f73375e52b5ce9f3cb97d10e16f50e071ef443
Marcin Dampc <marcindampc@gmail.com>
2019-08-06 03:24:08 +0100

    create sign in POST request endpoint

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require('express');
 
 const app = express();
 
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = new user object
+/profile/:userId --> GET = user information
+/image --> PUT = user score count
+*/
+
 app.get('/', (req, res) => {
   res.send('get is working');
 })
+
+app.post('/signin', (req, res) => {
+  res.send('sign in working');
+})
+
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
