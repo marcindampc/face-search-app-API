2d2fcaf384264678945895b7fb20946b1cb223cd
Marcin Dampc <marcindampc@gmail.com>
2019-09-15 05:38:05 +0100

    responding with single user after register and sending error message after catch one

--- server.js
+++ server.js
@@ -67,9 +67,10 @@ app.post('/register', (req, res) => {
     name: name,
     joined: new Date()
   } )
-    .then(response => {
-      res.json(response);
+    .then(user => {
+      res.json(user[0]);
     })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
