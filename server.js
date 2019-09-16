8f4a205c661786b9ad1ed65a1ef7b09c2e54d8a3
Marcin Dampc <marcindampc@gmail.com>
2019-09-16 17:52:51 +0100

    knex transaction - chaining two actions for succeed/fail together

--- server.js
+++ server.js
@@ -39,17 +39,30 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
   const hash = bcrypt.hashSync(password);
-  db('users')
-    .returning('*')
-    .insert({
-    email: email,
-    name: name,
-    joined: new Date()
-  } )
-    .then(user => {
-      res.json(user[0]);
+
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+      } )
+        .then(user => {
+          res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('unable to register'))
 })
 
 // get profile won't be used for now (future development)
