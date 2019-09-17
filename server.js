7c50d615e4671614c3a14b042f0fbc766c24341e
Marcin Dampc <marcindampc@gmail.com>
2019-09-17 03:31:16 +0100

    create controllers for server; dependecy injection case

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs'); // no longer actively maintained => switch to bcrypt || bcryptjs
 const cors = require('cors');
 const knex = require('knex');
+const register = require('./controllers/register');
 
 const db = knex({
   client: 'pg',
@@ -47,33 +48,8 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-        .returning('*')
-        .insert({
-          email: loginEmail[0],
-          name: name,
-          joined: new Date()
-      } )
-        .then(user => {
-          res.json(user[0]);
-        })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  .catch(err => res.status(400).json('unable to register'))
-})
+     register.handleRegister(req, res, db, bcrypt)
+    }) // DEPENDENCY INJECTION! of db & bcrypt
 
 // get profile won't be used for now (future development)
 app.get('/profile/:id', (req, res) => {
